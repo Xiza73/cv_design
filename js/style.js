214e72427e69ba0e34327ac4f0e95afd5dbeb54a
Xiza <m.fajardo.1098@gmail.com>
2021-04-05 17:41:23 -0500

    primer terminable cv

--- js/style.js
+++ js/style.js
@@ -1,24 +1,12 @@
 $(function () {
     $(window).scroll(function () {
         var winTop = $(window).scrollTop();
-        var winBfore = 0;
         if (winTop >= 30) {
             $("body").addClass("sticky-header");
             $("nav").css("visibility", "visible");
-            //$("main").css("position", "static");
-            /*if (winBfore === 0) {
-                $('html,body').animate({
-                    //the 205 is a fixed header offset
-                    scrollTop: $('#perfil').offset().top
-                }, 300);
-                return false;
-            }*/
-
         } else {
             $("body").removeClass("sticky-header");
             $("nav").css("visibility", "hidden");
-            //$("main").css("position", "relative");
-            winBfore = 0;
         } //if-else
     }); //win func.
 }); //ready func.
